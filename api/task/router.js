75c5722d468d51b63005947ecab65ef6b9ad3812
unknown <macrowell@yahoo.com>
2022-06-17 18:28:11 -0400

    implemented middleware for validating project id exists

--- api/task/router.js
+++ api/task/router.js
@@ -1,15 +1,26 @@
 const express = require('express');
 
 const Tasks = require('./model');
+const Projects = require('../project/model');
 
 const router = express.Router();
 
 const validatePost = (req, res, next) => {
     const { task_description, project_id } = req.body
     if (!task_description || !project_id) {
-       return res.status(400).json({ message: "must have task_description and project_id" })
+       return res
+       .status(400)
+       .json({ message: "must have task_description and project_id" })
     }
     next()
+}
+
+const validateProjectId = (req, res, next) => {
+    Projects.getProjects(req.body.project_id)
+        .then(() => next())
+        .catch(err => res
+            .status(400)
+            .json({ message: "that project id does not exist" }))
 }
 
 router.get('/', (req, res) => {
@@ -18,7 +29,7 @@ router.get('/', (req, res) => {
     .catch(err => res.json(err.message))
 });
 
-router.post('/', validatePost, (req, res) => {
+router.post('/', validatePost, validateProjectId, (req, res) => {
     Tasks.insertTask(req.body)
         .then(task => res.json(task))
         .catch(err => res.json(err.message))
