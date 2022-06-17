e23f91f75e34ecd26ca97b2c5c61f5591b0bdd34
unknown <macrowell@yahoo.com>
2022-06-16 21:38:22 -0400

    wired server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// start your server here
+const express = require('express');
+
+const server = require('./api/server');
+
+const PORT = process.env.PORT || 9000;
+
+server.listen(PORT, () => {
+    console.log(`server is listening on ${PORT}`)
+})
