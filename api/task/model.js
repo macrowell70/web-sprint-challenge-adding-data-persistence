cd59bfc7846396d7764d049e1a77bffc767fa438
unknown <macrowell@yahoo.com>
2022-06-17 18:09:53 -0400

    implemented validation middleware for post endpoint on tasks router

--- api/task/model.js
+++ api/task/model.js
@@ -1,24 +1,41 @@
 const db = require('../../data/dbConfig');
 
-const getTasks = async () => {
-    const result = await db('tasks')
+const getTasks = async (task_id) => {
+    const result = !task_id ? 
+    await db('tasks')
         .leftJoin('projects', 'tasks.project_id', 'projects.project_id')
         .select('tasks.*', 'projects.project_name', 'projects.project_description')
+            :
+        await db('tasks').where({ task_id }).first();
 
-    const tasks = result.map(task => {
-        return {
-            task_id: task.task_id,
-            task_description: task.task_description,
-            task_notes: task.task_notes,
-            task_completed: task.task_completed ? true : false,
-            project_name: task.project_name,
-            project_description: task.project_description
-        }
-    })
+    const tasks = !task_id ? 
+        result.map(task => {
+            return {
+                task_id: task.task_id,
+                task_description: task.task_description,
+                task_notes: task.task_notes,
+                task_completed: task.task_completed ? true : false,
+                project_name: task.project_name,
+                project_description: task.project_description
+            }
+            }) : {
+                task_id: result.task_id,
+                task_description: result.task_description,
+                task_notes: result.task_notes,
+                task_completed: result.task_completed ? true : false,
+                project_id: result.project_id
+            }
+
 
     return tasks
 }
 
+const insertTask = async (task) => {
+    const [task_id] = await db('tasks').insert(task)
+    return getTasks(task_id)
+}
+
 module.exports = {
     getTasks,
+    insertTask
 }
--- api/task/router.js
+++ api/task/router.js
@@ -4,14 +4,24 @@ const Tasks = require('./model');
 
 const router = express.Router();
 
+const validatePost = (req, res, next) => {
+    const { task_description, project_id } = req.body
+    if (!task_description || !project_id) {
+       return res.status(400).json({ message: "must have task_description and project_id" })
+    }
+    next()
+}
+
 router.get('/', (req, res) => {
    Tasks.getTasks()
     .then(tasks => res.json(tasks))
     .catch(err => res.json(err.message))
 });
 
-router.post('/', (req, res) => {
-    res.json({ message: "POST tasks router "})
+router.post('/', validatePost, (req, res) => {
+    Tasks.insertTask(req.body)
+        .then(task => res.json(task))
+        .catch(err => res.json(err.message))
 });
 
 module.exports = router
